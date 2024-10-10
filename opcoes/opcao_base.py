0513b88741b05f01c42540a18dce4aa8125fb07b
SSB-11 <shelles.borges@gmail.com>
2024-10-10 16:28:56 -0300

    feat: função de payoff

--- opcoes/opcao_base.py
+++ opcoes/opcao_base.py
@@ -8,7 +8,7 @@ class Opcao(ABC):
         
         Args:
         strike (float): Preço de exercício (strike).
-        premio (float): Prêmio pago pela opção.
+        premio (float): Prêmio pago ou recebido pela opção.
         quantidade (int): Quantidade de opções compradas (quantidade > 0) ou vendidas (quantidade < 0) (padrão é 100).
         """
 
@@ -20,13 +20,26 @@ class Opcao(ABC):
 
 
     def conferir_atributos(self):
-        "Checar se quantidade é um número inteiro, strike é positivo, etc etc."
+        "Checar se quantidade é um número inteiro positivo, strike é positivo, etc etc."
         pass
 
 
     @abstractmethod
     def calcular_preco_vencimento(self, preco_acao):
         pass
+
+
+    def calcular_payoff(self, preco_acao):
+        """
+        Calcula o lucro ou prejuízo da opção, por cada opção negociada. Para achar o valor total calcule `payoff * quantidade`.
+        - Quando uma opção é comprada, paga-se um prêmio.
+        - Quando uma opção é vendida, recebe-se um prêmio.
+        """
+        if self.quantidade > 0:
+            return self.calcular_preco_vencimento(preco_acao) - self.premio
+        if self.quantidade < 0:
+            return self.premio - self.calcular_preco_vencimento(preco_acao)
+        else: return 0
 
 
     def __str__(self, tipo):
