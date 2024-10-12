2636938a580e628e77c33f57c18ba6b16d6aa2f2
SSB-11 <shelles.borges@gmail.com>
2024-10-12 14:45:10 -0300

    feat: adiciona estratégia de opções (várias opções ao mesmo tempo)

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import streamlit as st
 import plotly.graph_objects as go
 
 from opcoes.opcao import Call, Put
+from opcoes.estrategia import Estrategia
 
 # st.session_state.show_graphic = False
 # st.markdown("<h1 style='text-align: center;'>Gráfico Interativo</h1>", unsafe_allow_html=True)
 st.title("Gráfico Interativo")
 adicionar = st.button("Adicionar Opção")
+if not st.session_state.estrategia:
+    st.session_state.estrategia = Estrategia()
+# st.write(st.session_state.opcao)
+# st.write(st.session_state.estrategia.get_opcoes())
 
 @st.dialog("Adicione uma opção")
 def adicionar_opcao():
@@ -29,15 +34,22 @@ def adicionar_opcao():
     quantidade = st.number_input('Quantidade: ', min_value=1, step=100, value=100)
     submitted = st.button("Adicionar")
     if submitted:
-        st.session_state.opcao_info = {
-            'opcao': Call(strike, premio, operacao) if tipo_opcao == 'Call' else Put(strike, premio, operacao),
-            'strike': strike, 
-            'premio': premio,
-            'tipo_opcao': tipo_opcao,
-            'operacao': operacao, 
-            'quantidade': quantidade
-        }
-        st.session_state.show_graphic = True
+        opcao = (
+            Call(strike, premio, operacao, quantidade) 
+            if tipo_opcao == 'Call' 
+            else Put(strike, premio, operacao, quantidade)
+        )
+        # st.session_state.opcao = opcao
+        st.session_state.estrategia.adicionar_opcao(opcao)
+        # st.session_state.opcao_info = {
+        #     'opcao': Call(strike, premio, operacao) if tipo_opcao == 'Call' else Put(strike, premio, operacao),
+        #     'strike': strike, 
+        #     'premio': premio,
+        #     'tipo_opcao': tipo_opcao,
+        #     'operacao': operacao, 
+        #     'quantidade': quantidade
+        # }
+        # st.session_state.show_graphic = True
         st.rerun()
 
 if adicionar:
@@ -45,11 +57,12 @@ if adicionar:
 
 # if st.session_state.show_graphic == True:
 fig = go.Figure()
-opcao = st.session_state.opcao_info
-x = np.arange(opcao['strike'] - 2, 14, 0.01)
-y = opcao['opcao'].calcular_payoff(x)
-
-fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='Line Plot'))
+i = 1
+for opcao in st.session_state.estrategia.get_opcoes():
+    x = np.arange(opcao.strike - 2, 14, 0.01)
+    y = opcao.calcular_payoff(x)
+    fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=f'Opcão {i}'))
+    i += 1
 
 fig.update_yaxes(tickformat=".2f")
 fig.update_xaxes(tickformat=".2f")
@@ -68,56 +81,3 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig)
-
-
-
-# col1, col2 = st.columns([1, 2])
-# with col1:
-#     with st.form("opcao"):
-#         tipo_opcao = st.radio(
-#             'Tipo de opção:',
-#             ['Call', 'Put'],
-#             captions=['Opção de Compra', 'Opção de Venda']
-#         )
-#         operacao = st.radio(
-#             'Tipo de operação:',
-#             ['Compra', 'Venda'],
-#             captions=['Compra de Opção', 'Venda de Opção']
-#         )
-#         strike = st.number_input('Strike: ', min_value=0.01, step=0.01)
-#         premio = st.number_input('Prêmio: ', min_value=0.01, step=0.01)
-#         quantidade = st.number_input('Quantidade: ', min_value=1, step=100, value=100)
-#         submitted = st.form_submit_button("Adicionar")
-#         if submitted:
-#             st.session_state.opcao_info = {
-#                 'opcao': Call(strike, premio) if tipo_opcao == 'Call' else Put(strike, premio),
-#                 'strike': strike, 
-#                 'premio': premio,
-#                 'tipo_opcao': tipo_opcao,
-#                 'operacao': operacao, 
-#                 'quantidade': quantidade
-#             }
-# with col2:
-#     if submitted:
-#         fig = go.Figure()
-#         x = np.arange(strike - 2, 14, 0.01)
-#         y = st.session_state.opcao_info['opcao'].calcular_payoff(x)
-#         fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='Line Plot'))
-
-#         fig.update_layout(
-#             title={
-#                 'text': 'Resultado no Vencimento',
-#                 'x': 0.55,
-#                 'xanchor': 'center'
-#             },
-#             xaxis_title='Preço do Ativo (R$)',
-#             yaxis_title='Lucro/Prejuízo (R$)',
-#             width=800,
-#             height=600,
-#             template='plotly_dark'
-#         )
-
-#         st.plotly_chart(fig)
-# with col3:
-#     st.header("Column 3")
-#     st.write("Content 3")
