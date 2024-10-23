03de56d73d812b8f33a86e7813bb2cee6a684cd4
SSB-11 <shelles.borges@gmail.com>
2024-10-23 11:23:42 -0300

    feat: função de custo

--- opcoes/estrategia.py
+++ opcoes/estrategia.py
@@ -3,28 +3,38 @@ class Estrategia:
         self._opcoes = []
 
 
+    def get_opcoes(self):
+        return self._opcoes
+
+
     def adicionar_opcao(self, opcao):
         self._opcoes.append(opcao)
 
 
     def remover_opcao(self, opcao):
-        if opcao in self._opcoes:
+        if opcao in self.get_opcoes():
             self._opcoes.remove(opcao)
             return True
-        print("A opção especificada não está na estratégia.")
+        print('A opção especificada não está na estratégia.')
         return False 
 
 
-    def get_opcoes(self):
-        return self._opcoes
-
-    
     def limpar_estrategia(self):
         self._opcoes = []
 
-    
+        
     def calcular_payoff(self, preco_acao):
         payoff = 0
         for opcao in self.get_opcoes():
             payoff += opcao.calcular_payoff(preco_acao)
         return payoff
+
+
+    def calcular_custo(self):
+        custo = 0
+        for opcao in self.get_opcoes():
+            if opcao.operacao == 'Compra':
+                custo += opcao.premio
+            else:
+                custo -= opcao.premio
+        return custo
